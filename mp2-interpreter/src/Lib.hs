a0783d14099efca031c9aa4a283c9646c334a341
Michael Wang <mwang748@gmail.com>
2026-03-02 01:37:41 +0000

    complete mp2

--- mp2-interpreter/src/Lib.hs
+++ mp2-interpreter/src/Lib.hs
@@ -1,7 +1,6 @@
 module Lib where
 import Data.HashMap.Strict as H (HashMap, empty, fromList, insert, lookup, union)
 
-
 --- Data Types
 --- ----------
 
@@ -88,10 +87,12 @@ liftIntOp op (IntVal x) (IntVal y) = IntVal $ op x y
 liftIntOp _ _ _ = ExnVal "Cannot lift"
 
 liftBoolOp :: (Bool -> Bool -> Bool) -> Val -> Val -> Val
-liftBoolOp = undefined
+liftBoolOp op (BoolVal a) (BoolVal b) = BoolVal $ op a b
+liftBoolOp _ _ _ = ExnVal "Cannot lift"
 
 liftCompOp :: (Int -> Int -> Bool) -> Val -> Val -> Val
-liftCompOp = undefined
+liftCompOp op (IntVal a) (IntVal b) = BoolVal $ op a b
+liftCompOp _ _ _ = ExnVal "Cannot lift"
 
 --- Eval
 --- ----
@@ -100,36 +101,73 @@ eval :: Exp -> Env -> Val
 
 --- ### Constants
 
-eval (IntExp i)  _ = undefined
-eval (BoolExp i) _ = undefined
+eval (IntExp i)  _ = IntVal i
+eval (BoolExp i) _ = BoolVal i
 
 --- ### Variables
 
-eval (VarExp s) env = undefined
+eval (VarExp s) env =
+    case H.lookup s env of
+        Nothing -> ExnVal "No match in env"
+        Just v -> v
 
 --- ### Arithmetic
 
-eval (IntOpExp op e1 e2) env = undefined
+eval (IntOpExp op e1 e2) env | (op == "/") && (eval e2 env == IntVal 0) = ExnVal "Division by 0"
+                             | otherwise =
+                                let
+                                    Just v = H.lookup op intOps
+                                in
+                                    liftIntOp v (eval e1 env) (eval e2 env)
 
 --- ### Boolean and Comparison Operators
 
-eval (BoolOpExp op e1 e2) env = undefined
+eval (BoolOpExp op e1 e2) env = let
+                                    Just v = H.lookup op boolOps
+                                in
+                                    liftBoolOp v (eval e1 env) (eval e2 env)
 
-eval (CompOpExp op e1 e2) env = undefined
+eval (CompOpExp op e1 e2) env = let
+                                    Just v = H.lookup op compOps
+                                in
+                                    liftCompOp v (eval e1 env) (eval e2 env)
 
 --- ### If Expressions
 
-eval (IfExp e1 e2 e3) env = undefined
+eval (IfExp e1 e2 e3) env = 
+                        case eval e1 env of
+                            BoolVal True -> eval e2 env
+                            BoolVal False -> eval e3 env
+                            _ -> ExnVal "Condition is not a Bool"
 
 --- ### Functions and Function Application
 
-eval (FunExp params body) env = undefined
+eval (FunExp params body) env = CloVal params body env
 
-eval (AppExp e1 args) env = undefined
+eval (AppExp e1 args) env = 
+    case eval e1 env of
+        CloVal params body env1 -> 
+            -- params: list of parameters, body: actual function body, env: environment
+            -- need to get the params into the body
+            -- params: [strings], body: exp
+            -- 1. zip the args into the params
+            -- 2. add to env1
+            -- 3. eval the body with
+            let newArgs = H.fromList (zip params (map (\a -> eval a env) args)) in
+                let newEnv = H.union newArgs env1 in
+                    eval body newEnv
+        _ -> ExnVal "Apply to non-closure"
 
 --- ### Let Expressions
 
-eval (LetExp pairs body) env = undefined
+eval (LetExp pairs body) env = 
+    -- take in a pairs [(variable, value)], expression on variable
+    -- add the result of the expression to the environment
+    -- put the value into corresponding place in the body and (eval body newenv)
+    let (vars, val) = unzip pairs in
+        let res = H.fromList (zip vars (map (\a -> eval a env) val)) in
+            let newEnv = H.union res env in
+                eval body newEnv
 
 --- Statements
 --- ----------
@@ -143,18 +181,43 @@ exec (PrintStmt e) penv env = (val, penv, env)
 
 --- ### Set Statements
 
-exec (SetStmt var e) penv env = undefined
+exec (SetStmt var e) penv env = 
+    let newVal = eval e env in
+        let newEnv = H.insert var newVal env in 
+            ("", penv, newEnv)
 
 --- ### Sequencing
 
-exec (SeqStmt []) penv env = undefined
+exec (SeqStmt []) penv env = ("", penv, env)
+exec (SeqStmt (x:xs)) penv env = 
+    let (res1, penv1, env1) = exec x penv env in
+        let (rest, penvr, envr) = exec (SeqStmt xs) penv1 env1 in 
+            (res1 ++ rest, penvr, envr)
 
 --- ### If Statements
 
-exec (IfStmt e1 s1 s2) penv env = undefined
+exec (IfStmt e1 s1 s2) penv env = 
+    case eval e1 env of
+        BoolVal True -> exec s1 penv env
+        BoolVal False -> exec s2 penv env
+        _ -> ("exn: Condition is not a Bool", penv, env)
 
 --- ### Procedure and Call Statements
 
-exec p@(ProcedureStmt name args body) penv env = undefined
+exec p@(ProcedureStmt name args body) penv env = 
+    -- need to add the stmt into the penv
+    -- add a stmt as the value into penv with key name
+    let newPenv = H.insert name p penv in
+        ("", newPenv, env)
 
-exec (CallStmt name args) penv env = undefined
+exec (CallStmt name args) penv env = 
+    -- same as appExp
+    -- evaluate the args
+    -- update the parameters and environments from the penv lookup
+    -- exec the body of the function on the new environment
+    case H.lookup name penv of
+        Nothing -> ("", penv, env)
+        Just (ProcedureStmt name params body) -> 
+                let res = H.fromList (zip params (map (\a -> eval a env) args)) in
+                    let newEnv = H.union res env in
+                        exec body penv newEnv
